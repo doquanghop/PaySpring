c21ed74b6d3ae3f7a65d610bba7444b22ba501d8
dqh999 <dqhit999@gmail.com>
2025-04-29 21:57:32 +0700

    first commit

---
+++ src/test/java/io/github/doquanghop/vnpay/VnpayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.doquanghop.vnpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VnpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
